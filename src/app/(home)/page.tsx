591ac980017500075b7626d34cbf29b59574ff17
Renan Molinari <ibarenan@gmail.com>
2024-02-01 15:14:53 -0300

    feat: add welcome message on home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,7 +1,18 @@
 import { Header } from "@/components/Header";
+import { format } from 'date-fns'
+import { ptBR } from "date-fns/locale";
 
 export default function Home() {
   return (
-    <Header />
+    <div>
+      <Header />
+      
+      <div className="px-5 pt-5">
+        <h2 className="text-xl font-bold">Olá, Miguel!</h2>
+        <p className="capitalize text-sm">
+          {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
+        </p>
+      </div>
+    </div>
   );
 }
