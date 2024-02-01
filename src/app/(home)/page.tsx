614565b0bc8626ef0f5efa622cc7daf681239625
Renan Molinari <ibarenan@gmail.com>
2024-02-01 15:21:46 -0300

    feat: add initial search component

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,22 @@
 import { Header } from "@/components/Header";
 import { format } from 'date-fns'
 import { ptBR } from "date-fns/locale";
+import { Search } from "./components/Search";
 
 export default function Home() {
   return (
     <div>
       <Header />
-      
+
       <div className="px-5 pt-5">
         <h2 className="text-xl font-bold">Olá, Miguel!</h2>
         <p className="capitalize text-sm">
           {format(new Date(), "EEEE',' dd 'de' MMMM", { locale: ptBR })}
         </p>
+      </div>
+
+      <div className="px-5 mt-6">
+        <Search />
       </div>
     </div>
   );
