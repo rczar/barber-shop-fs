3e6643316de5e14f6af229410e42056ee92e3842
Renan Molinari <ibarenan@gmail.com>
2024-02-02 14:51:09 -0300

    fix: search button

--- src/app/(home)/components/Search.tsx
+++ src/app/(home)/components/Search.tsx
@@ -8,7 +8,7 @@ export function Search() {
   return (
     <div className="flex items-center gap-2">
       <Input placeholder="Busque por uma barbearia" />
-      <Button variant="default" size="icon">
+      <Button variant="default" size="sm">
         <SearchIcon size={20} />
       </Button>
     </div>
